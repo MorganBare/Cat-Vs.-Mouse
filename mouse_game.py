492a865060e1e4b0aee924d15b95110d1f399e90
MorganBare <morganjbare@gmail.com>
2020-11-16 19:24:04 +0100

    Cleaned up some methods

--- mouse_game.py
+++ mouse_game.py
@@ -21,17 +21,21 @@ class MouseGame:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        # Responds to keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.cat.blitme()
-            
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+    
+    def _update_screen(self):
+         # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.cat.blitme()
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance and run the game
