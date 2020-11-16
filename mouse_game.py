5b469706e8690b7eaf07e38a01cb2dfc27d99237
MorganBare <morganjbare@gmail.com>
2020-11-16 16:54:49 +0100

    Added settings and background color

--- mouse_game.py
+++ mouse_game.py
@@ -1,14 +1,18 @@
+
 import sys
 
 import pygame
+
+from settings import Settings
 
 class MouseGame:
     """Overall class to manage game assets and behavior"""
     def __init__(self):
         """Initialize the game and create game resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Macka Mouse Game")
 
     def run_game(self):
@@ -18,6 +22,9 @@ class MouseGame:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
             
             # Make the most recently drawn screen visible
             pygame.display.flip()
