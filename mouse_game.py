1326a70685cfd52f2145b5abdad9bef78b155f18
MorganBare <morganjbare@gmail.com>
2020-11-29 11:13:00 +0100

    play button area has been deactivated and cursor is hidden during the game

--- mouse_game.py
+++ mouse_game.py
@@ -85,10 +85,14 @@ class MouseGame:
 
     def _check_play_button(self, mouse_pos):
         # Start a new game when the player clicks play
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active: 
             # Reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Hide the mouse cursor
+            pygame.mouse.set_visible(False) 
 
             # Get rid of any remaining mice and cheese
             self.mice.empty()
@@ -182,7 +186,8 @@ class MouseGame:
             # Pause
             sleep(0.5)     
         else:
-            self.stats.game_active = False       
+            self.stats.game_active = False   
+            pygame.mouse.set_visible(True)    
 
     def _check_pack_edges(self):
         """Respond if any mice touch the edges"""
